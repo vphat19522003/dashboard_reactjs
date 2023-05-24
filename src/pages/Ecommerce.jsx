2ee467b932043ad9f0b69fd26f9f5a8d3785c38d
vphat19522003 <19522002@gm.uit.edu.vn>
2023-05-24 16:19:11 +0700

    Add Ecommerce page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -7,8 +7,18 @@ import {
   SparklineAreaData,
   ecomPieChartData,
 } from "../data/dummy";
+
 const Ecommerce = () => {
-  return <div>Ecommerce</div>;
+  return (
+    <div className="mt-12">
+      <div className="flex flex-wrap justify-center lg:flex-nowrap">
+        <div className="w-full p-8 m-3 bg-white bg-center bg-no-repeat bg-cover dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl lg:w-80 pt-9 bg-hero-pattern"></div>
+        <div className="flex items-center justify-between">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Ecommerce;
